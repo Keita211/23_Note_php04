43a16ec3d0a35d098a8ff9423c711508cb94ef7a
Keita Note <keitanote211@gmail.com>
2021-07-15 01:43:18 +0900

    final

--- index.php
+++ index.php
@@ -21,26 +21,71 @@
                 
             <div class="navbar-header"><a class="navbar-brand" href="welcome.html">TOPページ</a></div>
                 <div id="data" class="navbar-header"><a class="navbar-brand" href="select.php">データ一覧</a></div>             
+                <div id="data" class="navbar-header"><a class="navbar-brand" href="select.php">画像一覧</a></div>             
             </div>
         </nav>
     </header>
 
     
-    <form method="POST" action="insert.php">
+    <form >
         <div class="jumbotron">
             <fieldset>
-                <legend>映画メモ</legend>
-                <label>映画名：<input type="text" name="name"></label><br>
-                <label>日付：<input type="date" name="date"></label><br>
-                <label>URL：<input type="text" name="url"></label><br>
-                <label>点数：<input type="number" name="score" max="5", min="1"></label><br>
-                <label>公開範囲：<input type="radio" name="open_flg"  value="0">有料会員のみ</label>
-                <label><input type="radio" name="open_flg" value ="1">全会員</label><br>
-                <label>感想：<br><textarea name="content" rows="4" cols="40"></textarea></label><br>
-                <input type="submit" value="送信">
+                <legend id="title">映画メモ</legend>
+                <label>映画名：<input type="text" id="name"></label><br>
+                <label>日付：<input type="date" id="date"></label><br>
+                <label>URL：<input type="text" id="url"></label><br>
+                <label>点数：<input type="number" id="score" max="5", min="1"></label><br>
+                <label>公開範囲: </label>
+                <select name="open_flg" id="open_flg">
+                    <option value="0">有料会員のみ</option>
+                    <option value="1">全会員</option>
+                </select><br>
+                <!-- <label>公開範囲：<input type="radio" id="open_flg"  value="0">有料会員のみ</label>
+                <label><input type="radio" id="open_flg2" value ="1">全会員</label><br> -->
+                <label>感想：<br><textarea id="content" rows="4" cols="40"></textarea></label><br>
             </fieldset>
         </div>
     </form>
+    <button id = "btn">登録</button>
+    <div><a href="fileupload.html">画像登録へ進む</a> </div>
+
+<!--Ajax-->
+
+<script>
+
+$("#btn").on("click", function(){
+    $.ajax({
+        type: "post",
+        url: "insert.php",
+        data:{
+            name:$("#name").val(),
+            date:$("#date").val(), 
+            url:$("#url").val(), 
+            score:$("#score").val(), 
+            open_flg:$("#open_flg").val(), 
+            content:$("#content").val(), 
+        },
+        datatype:"html",
+        success:function(data){
+            if(data=="false"){
+                alert("エラー");
+            }else{
+            $("#name").val(""),
+            $("#date").val(""), 
+            $("#url").val(""), 
+            $("#score").val(""), 
+            $("#open_flg").val(""), 
+            $("#content").val(""),     
+            $("#title").append("登録成功！！");
+            
+        }
+    }
+    })
+});
+
+
+</script>
+
 </body>
 
 </html>
--- insert.php
+++ insert.php
@@ -1,3 +1,8 @@
+
+<?php
+ini_set('display_errors',1);
+?>;
+
 <?php
 
 // データ取得
